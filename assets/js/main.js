8e286192caa6cf5435d378b6c353a44f6157d7ca
alexrturner <50839467+alexrturner@users.noreply.github.com>
2022-12-03 15:18:30 +0800

    branch test? ?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
     console.log("On course for a Cool Change");
-});
 
-$('html').onclick(function (e) {
-    e.preventDefault();
-    $('.overlay').addClass('hidden');
-});
 
-function hideOverlay() {
-    $('.overlay').addClass('hidden');
-}
+    $('html').onclick(function (e) {
+        e.preventDefault();
+        $('.overlay').addClass('hidden');
+    });
 
-$('.overlay').on('click scroll', function () {
-    this.addClass('hidden');
-    this.off('click scroll');
+    function hideOverlay() {
+        $('.overlay').addClass('hidden');
+    }
+
+    $('.overlay').on('click scroll', function () {
+        this.addClass('hidden');
+        this.off('click scroll');
+    });
 });

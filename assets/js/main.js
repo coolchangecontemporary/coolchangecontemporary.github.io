623a61e3f7481b588abec8b8965058ca4fa06ac9
alexrturner <50839467+alexrturner@users.noreply.github.com>
2022-12-03 14:51:21 +0800

    Add initial work

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    console.log("On course for a Cool Change");
+});
+
+$('html').onclick(function (e) {
+    e.preventDefault();
+    $('.overlay').addClass('hidden');
+});
+
+function hideOverlay() {
+    $('.overlay').addClass('hidden');
+}
+
+$('.overlay').on('click scroll', function () {
+    this.addClass('hidden');
+    this.off('click scroll');
+});

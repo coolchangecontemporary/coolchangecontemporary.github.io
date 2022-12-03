86ab29842889e71f9328295cf2ec57eb46f2b51f
Alexander Jones <spiderland@outlook.com>
2022-12-03 16:04:39 +0800

    Add CONVENTIONS.md, add TODOs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(init);
 
 function init() {
 
+    // `.one()` unbinds this event listener after its first execution - 
+    // so this code will only execute once
+    // TODO lock the scrollbar until the fadeout is complete
     $('.overlay').one('click wheel', function () {
         $('.overlay').addClass('hidden');
     });

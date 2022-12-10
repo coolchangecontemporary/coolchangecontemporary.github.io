cfed9b59cff4f1957a648c9dda735da17b065dce
Alexander Jones <spiderland@outlook.com>
2022-12-10 14:02:03 +0800

    Fully implement crossfade

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,23 @@ function init() {
 
     // `.one()` unbinds this event listener after its first execution - 
     // so this code will only execute once
-    // TODO lock the scrollbar until the fadeout is complete
-    $('.overlay').one('click wheel', function () {
-        $('.overlay').addClass('hidden');
+    $(document).one('click wheel keydown', function (e) {
+        // Allow the link on the overlay to be clicked
+        if (e.type !== 'click') {
+            e.preventDefault();
+        } 
+        // bg color transition 0-3s
+        $('.bg-primary').addClass('bg-other');
+        // 1.5-3.5s, overlay text fade out
+        $('.acknowledgement').addClass('hidden');
+        // 4-4.7s, main body fades in
+        $('main').removeClass('removed');
+        setTimeout(() => {
+            $('main').removeClass('hidden');
+        }, 20);
+        // Fully remove the acknowledgement div (for accessibility reasons)
+        setTimeout(() => {
+            $('.acknowledgement').addClass('removed');
+        }, 5000);
     });
 }

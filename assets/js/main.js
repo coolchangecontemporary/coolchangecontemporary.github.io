7e63c417840b0319741447a004b7593366402539
Alexander Jones <spiderland@outlook.com>
2022-12-10 14:33:45 +0800

    Add transitionBetween

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,8 @@ function init() {
             $('main').removeClass('removed');
         }, 3000)
         setTimeout(() => {
-            // Make the main actually visible
+            // Make the main actually visible - this must be slightly delayed
+            // (by 'a handful of milliseconds' according to MDN) otherwise it won't apply
             $('main').removeClass('hidden');
         }, 3020);
         // Fully remove the acknowledgement div (for accessibility reasons)
@@ -27,4 +28,29 @@ function init() {
             $('.acknowledgement').addClass('removed');
         }, 5000);
     });
+}
+
+/*
+ * Hide `selectorA` and show `selectorB`. 
+ * 
+ * Elements selected by `selectorB` must have the classes `removed` and `hidden`.
+ */
+function transitionBetween(selectorA, selectorB, showTimeMsB) {
+
+    // 1.5-3.5s, overlay text fade out
+    $(selectorA).addClass('hidden');
+    // 4-4.7s, main body fades in
+    setTimeout(() => {
+        // Add the main back to the DOM
+        $(selectorB).removeClass('removed');
+    }, showTimeMsB)
+    setTimeout(() => {
+        // Make the main actually visible - this must be slightly delayed
+        // (by 'a handful of milliseconds' according to MDN) otherwise it won't apply
+        $(selectorB).removeClass('hidden');
+    }, showTimeMsB + 20);
+    // Fully remove the acknowledgement div (for accessibility reasons)
+    setTimeout(() => {
+        $(selectorA).addClass('removed');
+    }, showTimeMsB + 2000);
 }
